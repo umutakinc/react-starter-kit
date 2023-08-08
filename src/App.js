29d9d7f695684e224bcce958da0bdfbf2759a016
Umut Akıncı <umut.akinci@medyasoft.com.tr>
2023-08-08 13:45:23 +0300

    stil işlemleri

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg'
+import Test from "./Test"
+import { Title } from "./Components"
+import Bootstrap from './Bootstrap'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <h4>{process.env.NODE_ENV}</h4>
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme='dark'>{process.env.NODE_ENV}</Title>
       <p>{process.env.REACT_APP_API_URL}</p>
+      <Test />
       {process.env.NODE_ENV === 'production' && (
         <>
           <img src="/logo192.png" alt="" />
           <img src={logo} alt="" />
         </>
       )}
+      <Bootstrap />
     </div>
   );
 }
---
+++ src/Bootstrap.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap';
+
+function Bootstrap() {
+    return (
+        <div>
+            <h3> Bootstrap Componentleri </h3>
+            <Button>Button</Button>
+            <Button variant='success'>Button</Button>
+        </div>
+    )
+}
+
+export default Bootstrap
